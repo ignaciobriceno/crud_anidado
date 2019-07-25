610f284eecee51d2b23106f64ae0e796bf3b4cc2
ignaciobriceno <ignacioob.10@gmail.com>
2019-07-25 19:38:45 -0400

    added project

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,21 @@
+class ProductsController < ApplicationController
+	def create
+		@product = Product.new(product_params)
+		@product.category = Category.find(params[:category_id])
+		@product.save
+	    redirect_to @product.category  
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		respond_to do |format|
+      		format.html { redirect_to @product.category, notice: 'Product was successfully destroyed.' }
+      		format.json { head :no_content }
+    	end
+	end
+
+	def product_params
+      params.require(:product).permit(:name, :price, :category_id)
+    end
+end
